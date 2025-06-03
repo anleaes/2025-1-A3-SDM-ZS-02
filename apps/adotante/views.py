9aee12e6b62bcacfc4ea78805e286652cea724fe
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-03 20:59:12 -0300

    criado viewset para adotante

--- apps/adotante/views.py
+++ apps/adotante/views.py
@@ -4,3 +4,9 @@ from rest_framework import viewsets
 from .serializers import AdotanteSerializer
 
 # Create your views here.
+
+#class AdotanteViewSet
+# ViewSet para o modelo Adotante
+class AdotanteViewSet(viewsets.ModelViewSet):
+    queryset = Adotante.objects.all()
+    serializer_class = AdotanteSerializer
