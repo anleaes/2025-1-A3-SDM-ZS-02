2be0765f933d6f094c692af924f9d095ce1f7857
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-03 20:31:02 -0300

    estrutura inicial do models de adotante

--- apps/adotante/models.py
+++ apps/adotante/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Adotante(models.Model):
+    #estrutura do modelo Adotante a principio
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=11, unique=True)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=15, blank=True, null=True)
+    endereco = models.TextField(blank=True, null=True)
+
+  
