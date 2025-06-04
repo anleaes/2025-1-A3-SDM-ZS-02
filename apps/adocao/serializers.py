4cb31fa8a8764b3a46d23d00fcdac0b2db0253d4
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-04 19:11:31 -0300

    serializer completo ate o momento

--- apps/adocao/serializers.py
+++ apps/adocao/serializers.py
@@ -3,3 +3,18 @@ from apps.adotante.serializers import AdotanteSerializer
 from apps.abrigo.serializers import AbrigoSerializer
 from .models import AdocaoAbrigo
 
+class AdocaoSerializer(serializers.ModelSerializer):
+    adotante = AdotanteSerializer(source='adotante', read_only=True)
+    abrigo = AbrigoSerializer(source='abrigo', read_only=True)
+
+    class Meta:
+        model = AdocaoAbrigo
+        fields = [
+            'id',
+            'adotante',
+            'abrigo',
+            'status',
+            'data_solicitacao',
+            'data_atualizacao'
+        ]
+        read_only_fields = ['data_solicitacao', 'data_atualizacao']
