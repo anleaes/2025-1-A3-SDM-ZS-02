3a282859cc15fc93b67d333424633e4c4a0304a1
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-04 17:52:11 -0300

    urls de abrigo completa ate o momento

--- apps/abrigo/urls.py
+++ apps/abrigo/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+from .serializers import AbrigoSerializer
+
 app_name = 'abrigo'
 router = routers.DefaultRouter()
 router.register('', views.AbrigoViewSet, basename='abrigo')
