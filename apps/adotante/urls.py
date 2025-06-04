cb393c8fbdbaf54f0d82093f1df1e87aea831819
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-03 21:12:28 -0300

    adicionando mudanças em urls de adotante

--- apps/adotante/urls.py
+++ apps/adotante/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+
+app_name = 'adotante'
+
+router = routers.DefaultRouter()
+router.register('', views.AdotanteViewSet, basename='adotante')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
