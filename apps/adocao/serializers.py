eb47d3561932ae95c64a73faa0af4927c228024a
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-04 20:21:11 -0300

    mudanças no serializer de adocao

--- apps/adocao/serializers.py
+++ apps/adocao/serializers.py
@@ -2,10 +2,12 @@ from rest_framework import serializers
 from apps.adotante.serializers import AdotanteSerializer
 from apps.abrigo.serializers import AbrigoSerializer
 from .models import AdocaoAbrigo
+from apps.adotante.models import Adotante
+from apps.abrigo.models import Abrigo
 
 class AdocaoSerializer(serializers.ModelSerializer):
-    adotante = AdotanteSerializer(read_only=True)
-    abrigo = AbrigoSerializer(read_only=True)
+    adotante = serializers.PrimaryKeyRelatedField(queryset=Adotante.objects.all())
+    abrigo = serializers.PrimaryKeyRelatedField(queryset=Abrigo.objects.all())
 
     class Meta:
         model = AdocaoAbrigo
