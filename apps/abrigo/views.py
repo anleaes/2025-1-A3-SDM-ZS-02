3c36e944fe3570a02cfe5ee1ac9b779e9922f99b
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-04 17:46:02 -0300

    criada a view de abrigo

--- apps/abrigo/views.py
+++ apps/abrigo/views.py
@@ -4,3 +4,6 @@ from .serializers import AbrigoSerializer
 from django.shortcuts import render
 
 # Create your views here.
+class AbrigoViewSet(viewsets.ModelViewSet):
+    queryset = Abrigo.objects.all()
+    serializer_class = AbrigoSerializer
