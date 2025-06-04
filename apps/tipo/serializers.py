c4e2d848ba20987f69e6e63de56d1eda1e47e635
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-04 20:43:54 -0300

    serializer completo ate o momento

--- apps/tipo/serializers.py
+++ apps/tipo/serializers.py
@@ -1,2 +1,7 @@
 from .models import Tipo
 from rest_framework import serializers
+
+class TipoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tipo
+        fields = ['id', 'nome', 'descricao']
