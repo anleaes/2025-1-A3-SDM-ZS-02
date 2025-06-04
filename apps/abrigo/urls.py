019d9fa92673b5c38e57a0314a0e84eadae07d68
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-04 17:53:05 -0300

    modificação em urls.py

--- apps/abrigo/urls.py
+++ apps/abrigo/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
-from .serializers import AbrigoSerializer
 
 app_name = 'abrigo'
 router = routers.DefaultRouter()
