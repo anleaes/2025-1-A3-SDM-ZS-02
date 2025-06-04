4a9bfbc81519677e7a8039b71a67f4ad0081f66c
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-04 19:08:44 -0300

    adicionado imports para serializers de adocao

---
+++ apps/adocao/serializers.py
@@ -0,0 +1,5 @@
+from rest_framework import serializers
+from apps.adotante.serializers import AdotanteSerializer
+from apps.abrigo.serializers import AbrigoSerializer
+from .models import AdocaoAbrigo
+
