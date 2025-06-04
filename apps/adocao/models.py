bc6d192dd84b9dc252ce4f3022fec29c71670ceb
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-04 18:36:00 -0300

    models para adocao completo ate o momento

--- apps/adocao/models.py
+++ apps/adocao/models.py
@@ -3,3 +3,21 @@ from adotantes.models import Adotante
 from abrigos.models import Abrigo
 
 # Create your models here.
+class AdotanteAbrigo(models.Model):
+    status_choices = [
+        ('aceito', 'Aceito'),
+        ('rejeitado', 'Rejeitado'),
+        ('pendente', 'Pendente'),
+        
+    ]
+    adotante = models.ForeignKey(Adotante, on_delete=models.CASCADE, related_name='adotante_abrigos')
+    abrigo = models.ForeignKey(Abrigo, on_delete=models.CASCADE, related_name='abrigo_adotantes')
+    status = models.CharField(max_length=10, choices=status_choices, default='pendente')
+    class Meta:
+        verbose_name = 'Abrigo do Adotante'
+        verbose_name_plural = 'Abrigos dos Adotantes'
+        ordering = ['id']
+        unique_together = ('adotante', 'abrigo')
+
+    def __str__(self):
+        return f"{self.adotante.nome} - {self.abrigo.nome}"
