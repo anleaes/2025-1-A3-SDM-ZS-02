bb0495cb15380dc027212007634b0b61f551ec45
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-04 17:54:36 -0300

    urls.py de abrigo completo

--- adocao_project/urls.py
+++ adocao_project/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('adotante/', include('apps.adotante.urls')),
+    path('abrigo/', include('apps.abrigo.urls')),
 ]
