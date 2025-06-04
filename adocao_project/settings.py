871933dc1f5a66ba484428328bcaa5427bee7191
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-04 17:30:25 -0300

    adicionado o app de abrigo ao installed apps de settings

--- adocao_project/settings.py
+++ adocao_project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'apps.adotante.apps.AdotanteConfig',
+    'apps.abrigo.apps.AbrigoConfig',
     
 ]
 
--- apps/abrigo/apps.py
+++ apps/abrigo/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class AbrigoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'abrigo'
+    name = 'apps.abrigo'
+    verbose_name = 'Abrigo'
