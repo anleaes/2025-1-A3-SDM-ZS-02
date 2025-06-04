798c48b9629c39a8e932c5f1685ee0172a1d43f4
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-04 20:46:44 -0300

    views para tipo completa ate o momento

--- apps/tipo/views.py
+++ apps/tipo/views.py
@@ -3,3 +3,7 @@ from .models import Tipo
 from rest_framework import viewsets
 from .serializers import TipoSerializer
 # Create your views here.
+
+class TipoViewSet(viewsets.ModelViewSet):
+    queryset = Tipo.objects.all()
+    serializer_class = TipoSerializer
