f955f61c83f77903055e44859be9ec6cf28a8d9e
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-04 19:36:57 -0300

    views completo ate o momento

--- apps/adocao/views.py
+++ apps/adocao/views.py
@@ -4,3 +4,6 @@ from .models import AdocaoAbrigo
 from .serializers import AdocaoSerializer
 
 # Create your views here.
+class AdocaoViewSet(viewsets.ModelViewSet):
+    queryset = AdocaoAbrigo.objects.all()
+    serializer_class = AdocaoSerializer
