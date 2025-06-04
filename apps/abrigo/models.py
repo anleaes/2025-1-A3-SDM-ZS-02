2545dc7a5f6e2a2e150681b0c10ecab33af4fdcb
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-04 17:35:07 -0300

    criada estrutura basica do models de abrigo

--- apps/abrigo/models.py
+++ apps/abrigo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Abrigo(models.Model):
+    nome = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=15, blank=True, null=True)
+    
