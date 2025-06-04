1bd533f7f8c2ed52a1b947935a5ce7d4078a2865
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-04 17:36:48 -0300

    estrutura de models completa ate o momento para abrigo

--- apps/abrigo/models.py
+++ apps/abrigo/models.py
@@ -6,4 +6,12 @@ class Abrigo(models.Model):
     nome = models.CharField(max_length=100)
     endereco = models.CharField(max_length=255)
     telefone = models.CharField(max_length=15, blank=True, null=True)
-    
+    email = models.EmailField(blank=True, null=True)
+    cnpj = models.CharField(max_length=14, unique=True, blank=True, null=True, default=None)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = "Abrigo"
+        verbose_name_plural = "Abrigos"
