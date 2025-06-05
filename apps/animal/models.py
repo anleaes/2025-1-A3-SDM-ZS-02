abeff31e109d3a103fa4566b07bf205cd2150a1c
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-05 20:22:57 -0300

    models para animal pronto ate o momento

--- apps/animal/models.py
+++ apps/animal/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 from apps.tipo.models import Tipo
 # Create your models here.
+class Animal(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=50, verbose_name='Nome do Animal')
+    idade = models.PositiveIntegerField(verbose_name='Idade do Animal')
+    tipo = models.ForeignKey(
+        Tipo, 
+        on_delete=models.CASCADE,
+        related_name='animais',
+        verbose_name='Tipo de Animal'
+    )
+    descricao = models.TextField(blank=True, null=True, verbose_name='Descrição do Animal')
+
+    def __str__(self):
+        return f"{self.nome} ({self.tipo.nome})"
