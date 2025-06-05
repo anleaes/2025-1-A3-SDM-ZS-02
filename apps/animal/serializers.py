42b97a63600c0310cd72d5f814de76974582b3ab
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-05 20:30:11 -0300

    serializer de animal pronto ate o momento

--- apps/animal/serializers.py
+++ apps/animal/serializers.py
@@ -2,3 +2,6 @@ from .models import Animal
 from rest_framework import serializers
 
 # serializer para o modelo animal
+class Meta:
+    model = Animal
+    fields = '__all__'  # Inclui todos os campos do modelo
