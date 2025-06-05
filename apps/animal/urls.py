0feecd538f94936c0e11d7900bd6898fc9946b23
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-05 20:56:05 -0300

    urls.py para animal pronto ate o momento

--- apps/animal/urls.py
+++ apps/animal/urls.py
@@ -1,3 +1,14 @@
 from django.urls import path, include
 from . import views
-from rest_framework.routers import Router
+from rest_framework import Router
+
+# Criação url para o app animal
+
+app_name = 'animal'
+
+router = routers.DefaultRouter()
+router.register('', views.AnimalViewSet, basename='animal')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    ]
