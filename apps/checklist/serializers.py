c028b7e953171d092007ba8d1308a5d332c1ed90
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-06 20:06:28 -0300

    mudanças no serializers.py de checklist

--- apps/checklist/serializers.py
+++ apps/checklist/serializers.py
@@ -2,6 +2,9 @@ from .models import Checklist
 from rest_framework import serializers
 
 class ChecklistSerializer(serializers.ModelSerializer):
+    animal_nome = serializers.CharField(source='animal.nome', read_only=True)
+    vacina_nome = serializers.CharField(source='vacina.nome', read_only=True)
+    
     class Meta:
         model = Checklist
-        fields = '__all__'
+        fields = ['id','animal_nome', 'vacina_nome','vacina', 'data_checklist']
