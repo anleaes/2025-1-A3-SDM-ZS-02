3c0a2f21dec4a384488a3a9c49d912636d93e172
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-06 17:20:49 -0300

    imports iniciais para adocaoitem

--- apps/adocaoitem/views.py
+++ apps/adocaoitem/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from apps.adocaoitem.models import AdocaoItem
+from .serializers import AdocaoItemSerializer
+from rest_framework import viewsets
 
 # Create your views here.
