a08773b0554efde570c963c7688b0ea04962b581
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-06 17:21:43 -0300

    views para adocaoitem prontas ate o momento

--- apps/adocaoitem/views.py
+++ apps/adocaoitem/views.py
@@ -4,3 +4,6 @@ from .serializers import AdocaoItemSerializer
 from rest_framework import viewsets
 
 # Create your views here.
+class AdocaoItemViewSet(viewsets.ModelViewSet):
+    queryset = AdocaoItem.objects.all()
+    serializer_class = AdocaoItemSerializer
