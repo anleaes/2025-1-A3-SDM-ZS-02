fc73c0eb8501c69e990f4f6d72666d7cf6d7e072
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-06 19:09:35 -0300

    urls.py de vacina prontos ate o momento

--- apps/vacina/urls.py
+++ apps/vacina/urls.py
@@ -2,3 +2,9 @@ from django.urls import path, include
 from rest_framework.routers import routers
 from .views import VacinaViewSet
 
+app = 'vacina'
+router = routers.DefaultRouter()
+router.register('', VacinaViewSet, basename='vacina')
+urlpatterns = [
+    path('', include(router.urls)),
+]
