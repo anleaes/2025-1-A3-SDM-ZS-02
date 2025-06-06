ade7df40996fc29a570562fd4a846dbe92413ca4
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-06 17:26:43 -0300

    urls para adocaoitem prontas ate o momento

--- apps/adocaoitem/urls.py
+++ apps/adocaoitem/urls.py
@@ -1,2 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import routers
+from . import views
+
+app_name = 'adocaoitem'
+router = routers.DefaultRouter()
+router.register('', views.adocaoitem, basename='adocaoitem')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
