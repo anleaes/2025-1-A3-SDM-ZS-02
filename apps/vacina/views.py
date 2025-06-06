d3bbc8a8f684cb249beae71776cd1fe86c876239
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-06 19:07:15 -0300

    views.py de vacina pronto ate o momento

--- apps/vacina/views.py
+++ apps/vacina/views.py
@@ -3,3 +3,6 @@ from rest_framework import viewsets
 from .serializers import VacinaSerializer
 
 # Create your views here.
+class VacinaViewSet(viewsets.ModelViewSet):
+    queryset = VacinaSerializer.Meta.model.objects.all()
+    serializer_class = VacinaSerializer
