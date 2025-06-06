4e49b069d4e82d203ef4b146508c58b2865f88b5
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-06 19:54:35 -0300

    retirada a data de vacinação de vacina pois acredito fazer mais sentido estar em checklist

--- apps/vacina/models.py
+++ apps/vacina/models.py
@@ -4,11 +4,10 @@ from django.db import models
 class Vacina(models.Model):
     nome = models.CharField(max_length=100, unique=True, verbose_name='Nome da Vacina')
     descricao = models.TextField(blank=True, null=True, verbose_name='Descrição da Vacina')
-    data_vacinacao = models.DateField(verbose_name='Data de Vacinação')
 
     class Meta:
         verbose_name = 'Vacina'
         verbose_name_plural = 'Vacinas'
 
     def __str__(self):
-        return f"{self.nome} - {self.data_vacinacao.strftime('%d/%m/%Y')} - {self.descricao[:50]}"
+        return f"{self.nome} - {self.descricao[:50]}"
