43363348284591d3261e533e7ae6043ab09825ef
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-06 17:14:39 -0300

    serializers de adocaoitem pronto ate o momento

--- apps/adocaoitem/serializers.py
+++ apps/adocaoitem/serializers.py
@@ -1,2 +1,7 @@
 from .models import AdocaoItem
 from rest_framework import serializers
+class AdocaoItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AdocaoItem
+        fields = '__all__'
+        read_only_fields = ('id', 'adocao', 'animal')
