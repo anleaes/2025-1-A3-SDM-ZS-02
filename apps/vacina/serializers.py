2f97f69630ae1d1f164915e97755fe447c0d3f5e
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-06 19:20:55 -0300

    arrumando serializers.py de vacina e urls.py

--- apps/vacina/serializers.py
+++ apps/vacina/serializers.py
@@ -1,7 +1,7 @@
-from .models import vacina
+from .models import Vacina
 from rest_framework import serializers
 
 class VacinaSerializer(serializers.ModelSerializer):
     class Meta:
-        model = vacina
+        model = Vacina
         fields = '__all__'
