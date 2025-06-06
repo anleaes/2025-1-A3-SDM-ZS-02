cda887a5fe565ca74535815fcb37e5362c5a62e7
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-06 19:40:07 -0300

    urls.py de checklist pronto ate o momento

--- apps/checklist/urls.py
+++ apps/checklist/urls.py
@@ -2,3 +2,10 @@ from django.urls import path, include
 from rest_framework import routers
 from .views import ChecklistViewSet
 
+app_name = 'checklist'
+router = routers.DefaultRouter()
+router.register('', ChecklistViewSet, basename='checklist')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
