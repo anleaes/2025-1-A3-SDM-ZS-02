e6fa63b1f909d3dde8e4e4bc8ddf451f78c0f8b1
MarcosCRosa <marcosluiz4012@gmail.com>
2025-06-06 19:00:25 -0300

    criada a class vacina no models de vacina

--- apps/vacina/models.py
+++ apps/vacina/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
 # Create your models here.
+
+class Vacina(models.Model):
+    nome = models.CharField(max_length=100, unique=True, verbose_name='Nome da Vacina')
+    descricao = models.TextField(blank=True, null=True, verbose_name='Descrição da Vacina')
+    data_vacinacao = models.DateField(verbose_name='Data de Vacinação')
+
+   
